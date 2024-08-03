031262d71b67a77082c6a85532b247d54c152f7c
christian cuaresma <leonardemmanuelcuaresma@gmail.com>
2024-08-03 20:44:31 +0800

    Updated vercel config

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const { registerRouter } = require("./routes/index.js");
-const port = 3000;
+const port = 3300;
 
 // parses incoming requests with JSON payloads
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Invitation-api"));
 
 app.use("/api", registerRouter);
 
